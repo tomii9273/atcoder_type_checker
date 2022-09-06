985fb31f9c09e3486ad2e8ebf188423aa0bc9b1f
Tomii9273 <tomii6614@yahoo.co.jp>
2022-09-06 22:53:32 +0900

    Revert "add filter"
    This reverts commit 99dd2d146d05ce7c5a3dfdb73fb6e95f7511cf88.

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-import re
-
 from flask import Flask, render_template, request
 
 from plot_result import plot_result
@@ -16,8 +14,7 @@ def get():
 # postのときの処理
 @app.route("/", methods=["POST"])
 def post():
-    raw_name = request.form["name"]
-    name = "".join(re.findall("[a-zA-Z0-9_]+", raw_name))
+    name = request.form["name"]
     score, rate2, times, first_score, mean_score = get_type(name)
     mes1 = ""
     mes2 = ""
