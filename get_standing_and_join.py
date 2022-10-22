b8470ed6f9771412021764b79b4b30d2a2b8c077
Tomii9273 <tomii6614@yahoo.co.jp>
2022-10-22 23:35:14 +0900

    fix bug

--- get_standing_and_join.py
+++ get_standing_and_join.py
@@ -12,7 +12,7 @@ import requests
 from bs4 import BeautifulSoup
 
 # パラメータここから
-contest_names = ["abc{i}" for i in range(268, 275)] + ["arc{i}" for i in range(148, 152)]
+contest_names = [f"abc{i}" for i in range(268, 275)] + [f"arc{i}" for i in range(148, 152)]
 # パラメータここまで
 
 pw = input("Password?: ")
