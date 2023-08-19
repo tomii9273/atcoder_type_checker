0b473d388f7b961da8db795d571ce88a37a4f851
Tomii9273 <tomii6614@yahoo.co.jp>
2023-08-19 09:27:46 +0900

    add hoseichi update pipeline

---
+++ update_hoseichi.py
@@ -0,0 +1,13 @@
+import sys
+
+from get_some_usernames import get_users_for_hosei
+from output_result_5n_and_top import get_type_for_hosei
+
+
+def make_new_hoseichi(file_name):
+    data = get_users_for_hosei()
+    get_type_for_hosei(data, file_name)
+
+
+if __name__ == "__main__":
+    make_new_hoseichi(sys.argv[1])
