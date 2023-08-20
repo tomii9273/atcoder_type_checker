95d920219537afc408bedb8931481a6de3e46599
Tomii9273 <tomii6614@yahoo.co.jp>
2023-08-21 06:40:04 +0900

    add argparse

--- update_hoseichi.py
+++ update_hoseichi.py
@@ -1,4 +1,4 @@
-import sys
+import argparse
 
 from get_some_usernames import get_users_for_hosei
 from output_result_5n_and_top import get_type_for_hosei
@@ -10,4 +10,9 @@ def make_new_hoseichi(file_name: str, debug: bool) -> None:
 
 
 if __name__ == "__main__":
-    make_new_hoseichi(file_name=sys.argv[1], debug=sys.argv[2])
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-n", "--name", type=str, required=True, help="新たに作成する補正値ファイルの名前")
+    parser.add_argument("-d", "--debug", action="store_true", help="デバッグ実行か (補正値が 100 データのみになる)")
+    args = parser.parse_args()
+
+    make_new_hoseichi(file_name=args.name, debug=args.debug)
