c52abf92cad265a8933985f6e112502ebcbe24d8
Tomii9273 <tomii6614@yahoo.co.jp>
2023-08-30 22:40:02 +0900

    fix path

--- app/src/const.py
+++ app/src/const.py
@@ -2,7 +2,7 @@
 DEGREE_OF_HOSEI_CURVE = 10
 
 # 算出に使う補正値 (の式の作成に用いる (内部レート, 平均順位率) 一覧) のファイルのパス
-HOSEICHI_FILE_PATH = "data/hoseichi/test.npy"
+HOSEICHI_FILE_PATH = "data/hoseichi/hoseichi_5ntop_20230829.npy"
 
 # 重みづけした平均順位率・スコアを取得するか
 USE_WEIGHTED_MEAN_RANK_RATE = True
